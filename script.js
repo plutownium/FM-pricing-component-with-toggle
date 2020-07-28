4a7e7c0c7647c5883ca0255fbb00aeecd15f56a6
plutownium <rolandmackintosh@outlook.com>
2020-07-28 15:13:59 -0700

    converting prices to use one span each instead of 2 spans

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const switchBox = document.getElementById("checkbox")
 
+const basicMtm = document.getElementById("basic-mtm-price")
+const basicYearly = document.getElementById("basic-yearly-price")
+const proMtm = document.getElementById("pro-mtm-price")
+const proYearly = document.getElementById("pro-yearly-price")
+const masterMtm = document.getElementById("master-mtm-price")
+const masterYearly = document.getElementById("master-yearly-price")
+
 console.log("hi")
 console.log(switchBox.checked)
 
@@ -7,7 +14,23 @@ switchBox.addEventListener("change", function () {
     console.log("this!", this)
     if (this.checked) {
         // switch to Full Year pricing
+        const monthToMonthDivs = document.getElementsByClassName("month-to-month")
+        for (const el of monthToMonthDivs) {
+            el.classList.add("hidden-property")
+        }
+        const yearlyDivs = document.getElementsByClassName("yearly")
+        for (const el of yearlyDivs) {
+            el.classList.remove("hidden-property")
+        }
     } else {
         // switch to Month-to-Month pricing
+        const monthToMonthDivs = document.getElementsByClassName("month-to-month")
+        for (const el of monthToMonthDivs) {
+            el.classList.remove("hidden-property")
+        }
+        const yearlyDivs = document.getElementsByClassName("yearly")
+        for (const el of yearlyDivs) {
+            el.classList.add("hidden-property")
+        }
     }
 })
